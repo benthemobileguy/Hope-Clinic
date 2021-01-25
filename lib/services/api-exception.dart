1e873ca60abc85686da736050c6952aa69a4b481
Ben <benbright065@gmail.com>
2021-01-25 06:42:36 +0100

    auth function

---
+++ lib/services/api-exception.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ApiException implements Exception {
+  int code;
+  String message;
+  Map<String, dynamic> errors;
+  bool preventRedirect;
+  BuildContext context;
+
+  ApiException(
+      {this.code,
+        this.message,
+        this.preventRedirect = false,
+        this.errors,
+        @required this.context}) {
+    switch (code) {
+      case 401:
+        break;
+      case 403:
+      // logOut();
+        break;
+      case 422:
+      case 400:
+        Fluttertoast.showToast(
+          msg: message,
+          backgroundColor: Colors.red,
+          timeInSecForIosWeb: 2,
+          toastLength: Toast.LENGTH_LONG,
+        );
+        break;
+      default:
+        Fluttertoast.showToast(msg: message, backgroundColor: Colors.red);
+        break;
+    }
+  }
+// void logOut() {
+//   PrefManager prefManager = PrefManager();
+//   prefManager.clearSharedPreferences().then((value) => ({
+//     Navigator.pushReplacement(
+//         context, SlideFromRightPageRoute(widget:
+//     LoginScreen()))
+//   }));
+// }
+}
+
