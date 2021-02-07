4c349c972bd389100ac373bce4dbf10646d19078
Ben <benbright065@gmail.com>
2021-02-07 13:59:59 +0100

    auth function

--- lib/screens/shop/components/shop-item.dart
+++ lib/screens/shop/components/shop-item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hope_clinic/theme/style.dart';
+
 class ShopItem extends StatelessWidget {
   const ShopItem({
     Key key,
@@ -7,41 +8,46 @@ class ShopItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount
-        (crossAxisCount: 2),
-
-      child: Column(
-        children: [
-          Container(
-              color: Colors.white,
-              child: new Image.asset('images/img_1.png',
-                width: 150, height: 180,)),
-          Text(
-            "Organic Ginger Balm",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontSize: 14,
-              fontFamily: 'Lato',
-              color: normalText,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            "5,000",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontSize: 16,
-              fontFamily: 'Lato',
-              color: primaryColor,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-
-        ],
+    return Expanded(
+      child: GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                  color: Colors.white,
+                  child: new Image.asset(
+                    'images/img_1.png',
+                    width: 150,
+                    height: 180,
+                  )),
+              Text(
+                "Organic Ginger Balm",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: normalText,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "5,000",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Lato',
+                  color: primaryColor,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
