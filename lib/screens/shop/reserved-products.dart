1de0156d396ecc42159fa125a8a3670a5eca02c9
Ben <benbright065@gmail.com>
2021-02-20 08:59:06 +0100

    reserved products

--- lib/screens/shop/reserved-products.dart
+++ lib/screens/shop/reserved-products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hope_clinic/bloc/index.dart';
+import 'package:hope_clinic/model/reserved-item.dart';
 import 'package:hope_clinic/theme/style.dart';
 class ReservedProducts extends StatefulWidget {
   @override
@@ -6,6 +8,16 @@ class ReservedProducts extends StatefulWidget {
 }
 
 class _ReservedProductsState extends State<ReservedProducts> {
+  List<ReservedItem> reservedItems;
+  MainBloc bloc;
+  bool isInitialised = false;
+  @override
+  void didChangeDependencies() async{
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    bloc = Provider.of<MainBloc>(context, listen: false);
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
