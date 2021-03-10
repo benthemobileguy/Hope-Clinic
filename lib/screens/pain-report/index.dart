7feb67cf73b44392e7a49ba2acd09a96472d01c6
Ben <benbright065@gmail.com>
2021-03-10 17:47:44 +0100

    pain report

--- lib/screens/pain-report/index.dart
+++ lib/screens/pain-report/index.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hope_clinic/theme/style.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 class PainReportPage extends StatefulWidget {
   @override
   _PainReportPageState createState() => _PainReportPageState();
@@ -7,22 +9,30 @@ class PainReportPage extends StatefulWidget {
 class _PainReportPageState extends State<PainReportPage> {
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      children: [
-        SizedBox(
-          height: 20,
+    return  SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "Touch where you feel the pain",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: textColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            SvgP
+          ],
         ),
-        Text(
-          "Touch where you feel the pain",
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            fontSize: 14,
-            fontFamily: 'Lato',
-            color: Colors.white,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
