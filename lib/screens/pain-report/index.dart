85ce73b18ba326d384a40cf02381a11464883195
Ben <benbright065@gmail.com>
2021-03-10 18:43:50 +0100

    pain report

--- lib/screens/pain-report/index.dart
+++ lib/screens/pain-report/index.dart
@@ -12,47 +12,81 @@ class _PainReportPageState extends State<PainReportPage> {
   Widget build(BuildContext context) {
     return  SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-              child: Text(
-                "Touch where you feel the pain",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 14,
-                  fontFamily: 'Lato',
-                  color: textColor,
-                  fontWeight: FontWeight.w400,
-                ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            Image.asset('images/person_front.png', height: 500, width: 272,),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              height: 60,
-              child: MainButton(
-                color: primaryColor,
+              Center(
                 child: Text(
-                  "Next",
+                  "Touch where you feel the pain",
                   textAlign: TextAlign.start,
                   style: TextStyle(
                     fontSize: 14,
                     fontFamily: 'Lato',
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
+                    color: textColor,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-                onPressed:null,
               ),
-            ),
-          ],
+              Stack(
+                  children: [
+                Image.asset('images/person_front.png', height: 500, width: 272,),
+                    Container(
+                      height: 60,
+                      child: MainButton(
+                        color: primaryColor,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            new Image.asset('images/icons/bookmark.png',
+                              height: 16,
+                              width: 10,),
+                            SizedBox(
+                              width: 14,
+                            ),
+                            Text(
+                              "Book Appointment",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: 'Lato',
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ],
+                        ),
+                        onPressed: null,
+                      ),
+                    ),
+                  ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                height: 60,
+                child: MainButton(
+                  color: primaryColor,
+                  child: Text(
+                    "Next",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: 'Lato',
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  onPressed:null,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
