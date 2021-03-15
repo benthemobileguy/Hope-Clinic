4efc6e99e0aaca08075fbc7863c8f7b8f5519eb1
Ben <benbright065@gmail.com>
2021-03-15 19:17:03 +0100

    pain report

--- lib/screens/pain-report/pain-report-desc-page.dart
+++ lib/screens/pain-report/pain-report-desc-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hope_clinic/theme/style.dart';
 class PainReportDescPage extends StatefulWidget {
   @override
   _PainReportDescPageState createState() => _PainReportDescPageState();
@@ -7,6 +8,45 @@ class PainReportDescPage extends StatefulWidget {
 class _PainReportDescPageState extends State<PainReportDescPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Pain Report",
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontSize: 16,
+            fontFamily: 'Lato',
+            color: textColor,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            size: 30,
+          ),
+          color: textColor,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Text(
+            "Extra Information",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 14,
+              fontFamily: 'Lato',
+              color: textColor,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
