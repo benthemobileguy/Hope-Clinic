ef347bb4765f83bb6096719a59f2cb7f53229eb8
Ben <benbright065@gmail.com>
2021-03-16 07:15:09 +0100

    pain report

--- lib/screens/home/full-image-screen.dart
+++ lib/screens/home/full-image-screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hope_clinic/bloc/index.dart';
+import 'package:hope_clinic/theme/style.dart';
+import 'package:path_provider/path_provider.dart';
 class FullImageScreen extends StatefulWidget {
   final int healthIndex;
 
@@ -8,8 +11,33 @@ class FullImageScreen extends StatefulWidget {
 }
 
 class _FullImageScreenState extends State<FullImageScreen> {
+  MainBloc mainBloc;
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    mainBloc = Provider
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: 400,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(color:
+              primaryColor.withOpacity(0.3),
+                  width: 2),
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    "${bloc.healthTips[healthIndex].files[index]}",
+                  ))),
+        ),
+      ),
+    );
   }
 }
