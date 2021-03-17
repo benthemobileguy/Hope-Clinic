3fc88339f2ad2565b9f89059e5e2226ca9ce3d26
Ben <benbright065@gmail.com>
2021-03-17 07:58:39 +0100

    pain report

--- lib/screens/message/index.dart
+++ lib/screens/message/index.dart
@@ -6,6 +6,7 @@ class MessageScreen extends StatefulWidget {
 }
 
 class _MessageScreenState extends State<MessageScreen> {
+  TextEditingController messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +33,54 @@ class _MessageScreenState extends State<MessageScreen> {
           },
         ),
       ),
-
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          children: [
+            Spacer(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    child: TextField(
+                      controller: messageController,
+                      cursorColor: textColor,
+                      maxLines: 10,
+                      decoration: InputDecoration(
+                          fillColor: containerBgColor,
+                          filled: true,
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'Lato',
+                            color: textColor,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12.0)),
+                          hintText: 'Type your message here'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  child: new Image.asset('images/send.png', width: 16,height: 16,),
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  ),
+                  width: 48,
+                  height: 50,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
