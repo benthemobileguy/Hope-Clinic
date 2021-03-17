3f9bb8c48e5a3b299a1f731dc95a563aa65799ed
Ben <benbright065@gmail.com>
2021-03-17 08:25:01 +0100

    pain report

--- lib/screens/message/index.dart
+++ lib/screens/message/index.dart
@@ -55,6 +55,76 @@ class _MessageScreenState extends State<MessageScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Column(
           children: [
+            Bubble(
+              alignment: Alignment.center,
+              color: Color.fromARGB(255, 212, 234, 244),
+              elevation: 1 * px,
+              margin: BubbleEdges.only(top: 8.0),
+              child: Text('TODAY', style: TextStyle(fontSize: 10)),
+            ),
+            Bubble(
+              style: styleSomebody,
+              child: Text('Hi Doctor, i just scheduled the appointment as you instructed',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: chatTextColor,
+                  fontWeight: FontWeight.normal,
+                ),),
+            ),
+            Bubble(
+              style: styleMe,
+              child: Text('Good to know! See you morning time',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: chatTextColor,
+                  fontWeight: FontWeight.normal,
+                ),),
+            ),
+            Bubble(
+              style: styleSomebody,
+              child: Text('Any requirement needed while coming?',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: chatTextColor,
+                  fontWeight: FontWeight.normal,
+                ),),
+            ),
+            Bubble(
+              style: styleSomebody,
+              margin: BubbleEdges.only(top: 2.0),
+              nip: BubbleNip.no,
+              child: Text('Not at all. Just come with your card',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: chatTextColor,
+                  fontWeight: FontWeight.normal,
+                ),),
+            ),
+            Bubble(
+              style: styleMe,
+              child: Text('Okay, Thanks',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Lato',
+                  color: chatTextColor,
+                  fontWeight: FontWeight.normal,
+                ),),
+            ),
+            Bubble(
+              style: styleMe,
+              nip: BubbleNip.no,
+              margin: BubbleEdges.only(top: 2.0),
+              child: Text('See you soon!', style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'Lato',
+                color: chatTextColor,
+                fontWeight: FontWeight.normal,
+              ),),
+            ),
             Spacer(),
             Row(
               mainAxisSize: MainAxisSize.min,
@@ -65,7 +135,6 @@ class _MessageScreenState extends State<MessageScreen> {
                     child: TextField(
                       controller: messageController,
                       cursorColor: textColor,
-                      maxLines: 10,
                       decoration: InputDecoration(
                           fillColor: containerBgColor,
                           filled: true,
@@ -85,15 +154,13 @@ class _MessageScreenState extends State<MessageScreen> {
                 SizedBox(
                   width: 10,
                 ),
-                Container(
-                  child: Center(child:
-                  Image.asset('images/send.png', width: 24,height: 24,)),
-                  decoration: BoxDecoration(
-                    color: primaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  width: 48,
-                  height: 50,
+                FlatButton(
+                  color: primaryColor,
+                  onPressed: (){
+                    messageController.text == "";
+                  },
+                  child: Image.asset('images/send.png',
+                    width: 24,height: 24,),
                 )
               ],
             ),
